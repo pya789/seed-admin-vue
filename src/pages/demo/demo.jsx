faf3c6a10a2709a6ab6d6fca69fba63ad9bff126
滑稽 <53637787@qq.com>
2022-08-15 18:55:16 +0800

    删除demo

--- src/pages/demo/demo.jsx
+++
@@ -1,9 +0,0 @@
-import {defineComponent} from 'vue'
-export default defineComponent({
-    setup() {
-        const message = "我是jsx页面哦"
-        return ()=>(
-            <div>{message}</div>
-        )
-    }
-})
